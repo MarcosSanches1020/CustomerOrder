ede525eeacb646cb0d11d782378678d6353ba08a
Markin Sanches <marcossanches710@gmail.com>
2025-09-22 16:46:12 -0300

    Ajustanto a configuração do Program.cs para que não ocorra o erro CORS quando rodar a chamada da api via Front

--- Program.cs
+++ Program.cs
@@ -60,6 +60,15 @@ namespace CustomerOrders.API
                 });
             });
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    policy => policy
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod() 
+                        .AllowAnyHeader()); 
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -73,6 +82,8 @@ namespace CustomerOrders.API
             {
                 app.UseHttpsRedirection();
             }
+
+            app.UseCors("AllowAll");
 
             app.UseAuthorization();
 
